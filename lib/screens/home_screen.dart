b7af0481648b968b95ac8ab8440479f3d44fbccf
Alejandro <alejandrohortalvalor02@gmail.com>
2024-11-08 08:58:46 +0100

    Hacemos desplegables para que lleven a los diferentes screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Componentes de Flutter",
+              style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.indigo,
+          elevation: 0, //Opcional para sombreado
+        ),
+        body: ListView.separated(
+          itemBuilder: (context, index) => ListTile(
+            leading: const Icon(Icons.accessibility_new_outlined),
+            title: const Text("Nombre de ruta"),
+            onTap: () {},
+          ),
+          separatorBuilder: (context, index) => const Divider(),
+          itemCount: 5,
+        ));
   }
 }
