b8ee18569d08c84625fa2bc04979b84a8fb1cdde
Alejandro Hortal <alejandrohortalvalor02@gmail.com>
2024-11-15 09:43:15 +0100

    añadir varias imágenes y pies de texto

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -15,7 +15,30 @@ class CardScreen extends StatelessWidget {
           children: const [
             CustomCardTipo1(),
             SizedBox(height: 10),
-            CustomCardTipo2()
+            CustomCardTipo2(
+                imageUrl:
+                    "https://img.goodfon.com/original/1920x1080/b/69/starkiller-by-sidezeo-star-wars-fog-sith-startroopers-weapon.jpg",
+                textUrl: 'StarKiller'),
+            SizedBox(height: 10),
+            CustomCardTipo2(
+                imageUrl:
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUXFResFkDQp4K7JII19mBNV9lAn7SCs6djQ&s",
+                textUrl: 'Darth Maul'),
+            SizedBox(height: 10),
+            CustomCardTipo2(
+                imageUrl:
+                    "https://images.squarespace-cdn.com/content/v1/5fbc4a62c2150e62cfcb09aa/1654399267221-JRCTJLTTJ4O9VSOH5SHJ/darth-revan-star-wars-closeup.jpg",
+                textUrl: 'Darth Revan'),
+            SizedBox(height: 10),
+            CustomCardTipo2(
+                imageUrl:
+                    "https://lumiere-a.akamaihd.net/v1/images/darth-vader-main_4560aff7.jpeg?region=0%2C67%2C1280%2C720",
+                textUrl: 'Darth Vader'),
+            SizedBox(height: 10),
+            CustomCardTipo2(
+                imageUrl:
+                    "https://lumiere-a.akamaihd.net/v1/images/anakin_skywalker_003_fbc652cd.jpeg?region=0,0,1379,809",
+                textUrl: 'Anakin Skywalker'),
           ]),
     );
   }
--- lib/widgets/custom_card_tipo_2.dart
+++ lib/widgets/custom_card_tipo_2.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 
 class CustomCardTipo2 extends StatelessWidget {
-  const CustomCardTipo2({super.key});
+  final String imageUrl;
+  final String textUrl;
+  const CustomCardTipo2(
+      {super.key, required this.imageUrl, required this.textUrl});
 
   @override
   Widget build(BuildContext context) {
     return Card(
         clipBehavior:
             Clip.antiAlias, //Si algo se sale del container, se cortará
+        shape: RoundedRectangleBorder(
+          // Para poner los bordes redondeados según tu quieras
+          borderRadius: BorderRadius.circular(15),
+        ),
         child: Column(
           children: [
-            const FadeInImage(
-              image: NetworkImage(
-                  "https://img.goodfon.com/original/1920x1080/b/69/starkiller-by-sidezeo-star-wars-fog-sith-startroopers-weapon.jpg"),
-              placeholder: AssetImage("assets/jar-loading.gif"),
+            FadeInImage(
+              // image: NetworkImage("https://img.goodfon.com/original/1920x1080/b/69/starkiller-by-sidezeo-star-wars-fog-sith-startroopers-weapon.jpg"),
+              image: NetworkImage(imageUrl),
+              placeholder: const AssetImage("assets/jar-loading.gif"),
               width: double.infinity, //Ocupa todo el ancho posible
               height: 260,
               fit: BoxFit.cover, // La relación aspecto sigue igual
-              fadeInDuration: Duration(
+              fadeInDuration: const Duration(
                   milliseconds: 3000), // La duración del desvanecimiento
             ),
             Container(
               alignment: AlignmentDirectional.centerEnd,
               padding: const EdgeInsets.only(top: 10, bottom: 10, right: 20),
-              child: const Text('StarKiller'),
+              child: Text(textUrl),
             ),
           ],
         ));
