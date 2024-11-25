d66b2d2b23934896c97b0918a84b0373490a0a69
Alejandro Hortal <alejandrohortalvalor02@gmail.com>
2024-11-25 10:42:52 +0100

    Descripciones añadidas a los cards en vez de el nombre de los bandos

--- lib/screens/casa_screen.dart
+++ lib/screens/casa_screen.dart
@@ -4,8 +4,12 @@ import 'package:fl_componentes/widgets/widget.dart';
 class CasaScreen extends StatelessWidget {
   final String imageUrl;
   final String textUrl;
-
-  const CasaScreen({super.key, required this.imageUrl, required this.textUrl});
+  final String descripcion;
+  const CasaScreen(
+      {super.key,
+      required this.imageUrl,
+      required this.textUrl,
+      required this.descripcion});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +20,7 @@ class CasaScreen extends StatelessWidget {
         body: ListView(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             children: [
-              CustomCardTipo2(imageUrl: imageUrl, textUrl: textUrl),
+              CustomCardTipo2(imageUrl: imageUrl, textUrl: descripcion),
             ]),
         floatingActionButton: FloatingActionButton(
             child: const Icon(Icons.close),
--- lib/screens/listview2_screen.dart
+++ lib/screens/listview2_screen.dart
@@ -9,6 +9,12 @@ class Listview2Screen extends StatelessWidget {
     "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Emblem_of_the_Rebel_Alliance.svg/220px-Emblem_of_the_Rebel_Alliance.svg.png",
     "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2019/02/first-order_8ae2955f.jpeg"
   ];
+  final descripciones = const [
+    "Los Jedi son caballeros que poseen un poder especial para controlar la Fuerza. La Orden Jedi reunió a estos protectores para mantener la paz y la justicia en la República Galáctica. Durante más de mil generaciones, los Jedi sirvieron a la República.",
+    "Los Sith, conocidos individualmente como Señores Sith, son, por naturaleza, despiadados. En cualquier momento, un solo individuo asume la autoridad absoluta entre los de su especie y se le otorga el título honorífico de Señor Oscuro de los Sith",
+    "La Alianza para Restaurar la República, comúnmente conocida como la Alianza Rebelde, la Alianza, la Rebelión, la Primera Rebelión, y rara vez los Separatistas, fue un movimiento de resistencia formado por Bail Organa y Mon Mothma para oponerse al reinado del Imperio Galáctico que los consideraba un conjunto de grupos ...",
+    "Las Fuerzas Armadas Imperiales eran una organización masiva, ya que Palpatine no hizo nada para detener la masiva acumulación militar que había producido un ejército de soldados y enormes armas de destrucción masiva después de las Guerras Clon."
+  ];
   const Listview2Screen({super.key});
 
   @override
@@ -26,6 +32,7 @@ class Listview2Screen extends StatelessWidget {
                   onTap: () {
                     final casa = options[index];
                     final imageUrl = imageUrls[index];
+                    final info = descripciones[index];
                     print(casa);
                     Navigator.push(
                       //Envia los datos a Casa Screen
@@ -34,6 +41,7 @@ class Listview2Screen extends StatelessWidget {
                         builder: (context) => CasaScreen(
                           imageUrl: imageUrl,
                           textUrl: casa,
+                          descripcion: info,
                         ),
                       ),
                     );
