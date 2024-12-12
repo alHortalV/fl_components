c82b62fd50f3bc0fa6c220d64f2a540039ee207f
Alejandro Hortal Valor <alejandrohortalvalor02@gmail.com>
2024-12-13 00:10:33 +0100

    fgj

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -6,16 +6,19 @@ class AppTheme {
   static final ThemeData lightTheme = ThemeData.light().copyWith(
       //Color primario
       primaryColor: primary,
+
       //App Bar Theme
       appBarTheme: const AppBarTheme(
           color: primary,
           titleTextStyle: TextStyle(
               color: secondary, fontSize: 20), //fontWeight: FontWeight.bold
           iconTheme: IconThemeData(color: secondary)),
+
       //ListTile Theme
       listTileTheme: const ListTileThemeData(
         iconColor: primary,
       ),
+
       //TextButton Theme
       textButtonTheme: TextButtonThemeData(
           style: TextButton.styleFrom(foregroundColor: primary)),
@@ -24,6 +27,7 @@ class AppTheme {
           elevation: 5,
           shape: StadiumBorder(),
           foregroundColor: secondary),
+
       //ElevatedButtons
       elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
@@ -31,6 +35,8 @@ class AppTheme {
               foregroundColor: secondary,
               shape: const StadiumBorder(),
               elevation: 5)),
+
+      //Input Decoration
       inputDecorationTheme: InputDecorationTheme(
         floatingLabelStyle: const TextStyle(color: primary),
         enabledBorder: OutlineInputBorder(
@@ -41,6 +47,8 @@ class AppTheme {
             borderRadius: BorderRadius.circular(10)),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
       ),
+
+      //Slider
       sliderTheme: const SliderThemeData(
         activeTrackColor: primary,
         thumbColor: primary,
